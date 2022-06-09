c1a76e04fc5fa12e304aa73e8c415d2c72591c4e
Yotam-D <d.yotam@gmail.com>
2022-06-09 18:07:00 +0300

    NavBar finished

---
+++ myteam-app/src/components/PageNavbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import Team from '../team.png';
+
+export default class PageNavbar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={Team}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+           /> MyTeam
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+              <Nav.Link href="#Home">Teams</Nav.Link>
+              <Nav.Link href="#pricing">DashBoard</Nav.Link>
+              </Nav>
+              <Nav>
+              <Nav.Link href="#deets">Login</Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    )
+  }
+}
